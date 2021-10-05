59b4689773d672106ab1890616e10d4d1a90850c
muhammad <iamrokt@gmail.com>
2021-10-05 12:53:02 +0200

    fix contact links

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -5,5 +5,5 @@
 <div class="links">
   Instagram: <a target="_blank" href="https://instagram.com/inrokt">@inrokt</a>
 Twitter: <a target="_blank" href="https://twitter.com/roktmy">@roktmy</a>
-  Discord: <a href="/" target="_blank">Coming soon!!</a>
+  Discord: <a href="/">Coming soon!!</a>
 </div>
