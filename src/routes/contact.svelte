64d406db23057205d894223ef834398ccc90f355
muhammad <iamrokt@gmail.com>
2021-10-05 12:44:22 +0200

    edit contact

---
+++ src/routes/contact.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <title>Speak Arabic - Contact</title>
+</svelte:head>
+
+<div class="links">
+  Instagram: <a href="https:instagram.com/inrokt">@inrokt</a>
+Twitter: <a href="https:twitter.com/roktmy">@roktmy</a>
+  Discord: <a href="/">Coming soon!!</a>
+</div>
