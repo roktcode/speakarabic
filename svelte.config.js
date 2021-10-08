bde50b03e48860655ad2f26ccc1353c537971517
muhammad <iamrokt@gmail.com>
2021-10-08 09:49:03 +0200

    added vercel support

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 /** @type {import('@sveltejs/kit').Config} */
 import node from "@sveltejs/adapter-node";
+import vercel from '@sveltejs/adapter-vercel'
 
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: "#svelte",
-		adapter: node({ env: { port: process.env.PORT } }),
+		adapter: vercel(),
+		// adapter: node({ env: { port: process.env.PORT } }),
 	},
 };
 
